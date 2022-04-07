56cde19d4342b2c5867902932d216b926c83d520
SM <SM@SM-PC>
2022-04-07 11:33:06 +0530

    Add project files.

---
+++ Asiingment5dayswitch/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter number");
+int num = Convert.ToInt32(Console.ReadLine());
+switch (num)
+{
+	case 1:
+        Console.WriteLine("Monday");
+		break;
+
+	case 2:
+		Console.WriteLine("Tuesday");
+		break;
+
+	case 3:
+		Console.WriteLine("Wednesday");
+		break;
+
+	case 4:
+		Console.WriteLine("Thursday");
+		break;
+
+	case 5:
+		Console.WriteLine("Friday");
+		break;
+
+	case 6:
+		Console.WriteLine("Saturday");
+		break;
+
+	case 7:
+		Console.WriteLine("Sunday");
+		break;
+}
